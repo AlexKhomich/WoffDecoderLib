0bd625cb25eb363e63bd0fb18774c699fec05ad6
Alex Khomich <khomich@scand.com>
2020-09-09 15:20:59 +0300

    Cleanup code.

--- src/structures.rs
+++ src/structures.rs
@@ -5,29 +5,28 @@ use crate::utils::{u32_to_u8_array, u16_to_u8_array};
 pub(crate) struct WoffHeader {
     // "magic number" - 0x774F4646 'wOFF'
     pub(crate) signature: u32,
-    //  (crate)The "sfnt version" of the input font
+    // The "sfnt version" of the input font
     pub(crate) flavor: u32,
-    //  (crate)Total size of the WOFF file
+    // Total size of the WOFF file
     pub(crate) length: u32,
-    //  (crate)Number of entries in directory of font tables
+    // Number of entries in directory of font tables
     pub(crate) num_tables: u16,
-    //  (crate)Reserved; set to zero
+    // Reserved; set to zero
     pub(crate) reserved: u16,
-    //  (crate)Total size needed for the uncompressed font data, including the sfnt header, directory, and font tables (including padding)
+    // Total size needed for the uncompressed font data, including the sfnt header, directory,
+    // and font tables (including padding)
     pub(crate) total_sfnt_size: u32,
-    //  (crate)Major version of the WOFF file
     pub(crate) major_version: u16,
-    //  (crate)Minor version of the WOFF file
     pub(crate) minor_version: u16,
-    //  (crate)Offset to metadata block, from beginning of WOFF file
+    // Offset to metadata block, from beginning of WOFF file
     pub(crate) meta_offset: u32,
-    //  (crate)Length of compressed metadata block
+    // Length of compressed metadata block
     pub(crate) meta_length: u32,
-    //  (crate)Uncompressed size of metadata block
+    // Uncompressed size of metadata block
     pub(crate) meta_orig_length: u32,
-    //  (crate)Offset to private data block, from beginning of WOFF file
+    // Offset to private data block, from beginning of WOFF file
     pub(crate) priv_offset: u32,
-    //  (crate)Length of private data block
+    // Length of private data block
     pub(crate) priv_length: u32,
 }
 
@@ -35,13 +34,13 @@ pub(crate) struct WoffHeader {
 pub(crate) struct WoffTableDirectoryEntry {
     // 4-byte sfnt table identifier
     pub(crate) tag: u32,
-    // (crate)Offset to the data, from beginning of WOFF file
+    // Offset to the data, from beginning of WOFF file
     pub(crate) offset: u32,
-    // (crate)Length of the compressed data, excluding padding
+    // Length of the compressed data, excluding padding
     pub(crate) comp_length: u32,
-    // (crate)Length of the uncompressed table, excluding padding
+    // Length of the uncompressed table, excluding padding
     pub(crate) orig_length: u32,
-    // (crate)Checksum of the uncompressed table
+    // Checksum of the uncompressed table
     pub(crate) orig_checksum: u32,
 }
 
@@ -49,13 +48,12 @@ pub(crate) struct WoffTableDirectoryEntry {
 pub(crate) struct SfntOffsetTable {
     // 0x00010000 or 0x4F54544F ('OTTO')
     pub(crate) version: u32,
-    // (crate)Number of tables.
     pub(crate) num_tables: u16,
-    // (crate)(Maximum power of 2 <= numTables) x 16.
+    // (Maximum power of 2 <= numTables) x 16.
     pub(crate) search_range: u16,
-    // (crate)Log2(maximum power of 2 <= numTables).
+    // Log2(maximum power of 2 <= numTables).
     pub(crate) entry_selector: u16,
-    // (crate)NumTables x 16-searchRange.
+    // NumTables x 16-searchRange.
     pub(crate) range_shift: u16,
 }
 
@@ -73,13 +71,10 @@ impl SfntOffsetTable {
 
 /// SFNT table record
 pub(crate) struct SfntTableRecord {
-    // Table identifier.
     pub(crate) table_tag: u32,
-    // (crate)CheckSum for this table.
     pub(crate) checksum: u32,
-    // (crate)Offset from beginning of TrueType font file.
+    // Offset from beginning of TrueType font file.
     pub(crate) offset: u32,
-    // (crate)Length of this table.
     pub(crate) length: u32,
 }
 
@@ -99,38 +94,37 @@ impl SfntTableRecord {
 pub(crate) struct SfntHeaderTable {
     // Major version number of the font header table — set to 1
     pub(crate) major_version: u16,
-    // (crate)Minor version number of the font header table — set to 0
+    // Minor version number of the font header table — set to 0
     pub(crate) minor_version: u16,
-    // (crate)Set by font manufacturer
+    // Set by font manufacturer
     pub(crate) font_revision: u32,
-    // (crate)Check sum
     pub(crate) check_sum_adjustment: u32,
-    // (crate)Set to 0x5F0F3CF5 "OTTO"
+    // Set to 0x5F0F3CF5 "OTTO"
     pub(crate) magic_number: u32,
-    // (crate)Flags
     pub(crate) flags: u16,
-    // (crate)Set to a value from 16 to 16384. Any value in this range is valid
+    // Set to a value from 16 to 16384. Any value in this range is valid
     pub(crate) units_per_em: u16,
-    // (crate)Number of seconds since 12:00 midnight that started January 1st 1904 in GMT/UTC time zone. 64-bit integer
+    // Number of seconds since 12:00 midnight that started January 1st 1904 in GMT/UTC time zone.
+    // 64-bit integer
     pub(crate) created: [u32; 2],
-    // (crate)Number of seconds since 12:00 midnight that started January 1st 1904 in GMT/UTC time zone. 64-bit integer
+    // Number of seconds since 12:00 midnight that started January 1st 1904 in GMT/UTC time zone.
+    // 64-bit integer
     pub(crate) modified: [u32; 2],
-    // (crate)For all glyph bounding boxes
+    // For all glyph bounding boxes
     pub(crate) x_min: i16,
-    // (crate)For all glyph bounding boxes
     pub(crate) y_min: i16,
-    // (crate)For all glyph bounding boxes
     pub(crate) x_max: i16,
-    // (crate)For all glyph bounding boxes
     pub(crate) y_max: i16,
-    // (crate)Bit 0: Bold (if set to 1); Bit 1: Italic (if set to 1) Bit 2: Underline (if set to 1) Bit 3: Outline (if set to 1) Bit 4: Shadow (if set to 1) Bit 5: Condensed (if set to 1) Bit 6: Extended (if set to 1) Bits 7–15: Reserved (set to 0).
+    // Bit 0: Bold (if set to 1); Bit 1: Italic (if set to 1) Bit 2: Underline (if set to 1)
+    // Bit 3: Outline (if set to 1) Bit 4: Shadow (if set to 1) Bit 5: Condensed (if set to 1)
+    // Bit 6: Extended (if set to 1) Bits 7–15: Reserved (set to 0).
     pub(crate) mac_style: u16,
-    // (crate)Smallest readable size in pixels.
+    // Smallest readable size in pixels.
     pub(crate) lowest_rec_ppem: u16,
-    // (crate)Deprecated. Only strongly left to right but also contains neutrals
+    // Deprecated. Only strongly left to right but also contains neutrals
     pub(crate) font_direction_hint: i16,
-    // (crate)0 for short offsets (Offset16), 1 for long (Offset32).
+    // 0 for short offsets (Offset16), 1 for long (Offset32).
     pub(crate) index_to_loc_format: i16,
-    // (crate)0 for current format.
+    // 0 for current format.
     pub(crate) glyph_data_format: i16,
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,38 +1,3 @@
-/* Tables tags list
-
-/* -- Tags for required TrueType tables */
-cmapTag = 0x636D6170; // 'cmap'
-glyfTag = 0x676C7966; // 'glyf'
-headTag = 0x68656164; // 'head'
-hheaTag = 0x68686561; // 'hhea'
-hmtxTag = 0x686D7478; // 'hmtx'
-locaTag = 0x6C6F6361; // 'loca'
-maxpTag = 0x6D617870; // 'maxp'
-nameTag = 0x6E616D65; // 'name'
-postTag = 0x706F7374; // 'post'
-prepTag = 0x70726570; // 'prep'
-os_2Tag = 0x4F532F32; // 'OS/2'
-
- /* -- Tags for opentype related tables */
-GDEFTag = 0x47444546; // 'GDEF'
-GPOSTag = 0x47504F53; // 'GPOS'
-GSUBTag = 0x47535542; // 'GSUB'
-mortTag = 0x6D6F7274; // 'mort'
-
- /* -- Tags for non-standard tables */
-fdscTag = 0x66647363; // 'fdsc' - gxFont descriptor
-fvarTag = 0x66766172; // 'fvar' - gxFont variations
-featTag = 0x66656174; // 'feat' - layout features
-EBLCTag = 0x45424C43; // 'EBLC' - embedded bitmaps
-gaspTag = 0x67617370; // 'gasp' - hint/smooth sizes
-
-    /* --  Other tags */
-ttcfTag = 0x74746366; // 'ttcf' - TTC file
-v1ttTag = 0x00010000; // 'v1tt' - Version 1 TT font
-trueTag = 0x74727565; // 'true' - Version 2 TT font
-ottoTag = 0x4f54544f; // 'otto' - OpenType font
-*/
-
 use std::io::Cursor;
 use byteorder::{BigEndian, ReadBytesExt};
 use std::fs::File;
